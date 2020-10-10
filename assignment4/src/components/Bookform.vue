975ab4ab9aca754d0a2cb8e8f6d2c0fae024e10d
Gulsonmahesh <mahesh.madasamy@wipro.com>
2020-10-10 22:47:48 +0530

    Converted the book page to Composition API

--- assignment4/src/components/Bookform.vue
+++ assignment4/src/components/Bookform.vue
@@ -1,24 +1,20 @@
 <template>
-  <div class="container-fluid m-5 p-0">
-    <div class="row">
-      <div class="column column-12">
-        <form class="form">
-            <div class="form-group d-block">
-                <label for="booktitle" class="mr-5">Enter Book Title</label>
-                <input autocomplete="false" v-model.trim="formData.bookname" type="text" class="form-control" id="booktitle" placeholder="Enter Book Title">
-            </div>
-            <div class="form-group d-block">
-                <label for="bookyear" class="mr-1">Enter Publication Year</label>
-                <input autocomplete="false" v-model.trim="formData.bookyear" type="number" min="2000" max="9999" pattern="/^-?\d+\.?\d*$/" onKeyPress="if(formData.value.length==4) return false;" class="form-control" id="bookyear" placeholder="Published Year" />
-            </div>
-            <div class="form-group d-block">
-                <label for="price" class="mr-1">Enter Price</label>
-                <input autocomplete="false" v-model.trim="formData.price" type="number" min="1" max="9999" pattern="/^-?\d+\.?\d*$/" onKeyPress="if(formData.value.length==4) return false;" class="form-control" id="price" placeholder="Price" />
-            </div>
-            <button @click.prevent="addbook" class="btn btn-primary">Add Book</button>
-        </form>        
-      </div>
-    </div>
+  <div class="container-fluid m-0 p-0">
+    <form class="form m-2">
+        <div class="form-group d-block">
+            <label for="booktitle" class="mr-5">Enter Book Title</label>
+            <input autocomplete="false" v-model.trim="formData.bookname" type="text" class="form-control" id="booktitle" placeholder="Enter Book Title">
+        </div>
+        <div class="form-group d-block">
+            <label for="bookyear" class="mr-1">Enter Publication Year</label>
+            <input autocomplete="false" v-model.trim="formData.bookyear" type="number" min="2000" max="9999" pattern="/^-?\d+\.?\d*$/" onKeyPress="if(formData.value.length==4) return false;" class="form-control" id="bookyear" placeholder="Published Year" />
+        </div>
+        <div class="form-group d-block">
+            <label for="price" class="mr-1">Enter Price</label>
+            <input autocomplete="false" v-model.trim="formData.price" type="number" min="1" max="9999" pattern="/^-?\d+\.?\d*$/" onKeyPress="if(formData.value.length==4) return false;" class="form-control" id="price" placeholder="Price" />
+        </div>
+        <button @click.prevent="addbook" class="btn btn-primary">Add Book</button>
+    </form>
   </div>
 </template>
 <script>
@@ -29,8 +25,8 @@ export default {
   setup(emit) {
     let formData = reactive({
       bookname: null,
-      bookyear : 2000,
-      price: 1
+      bookyear : '',
+      price: ''
     })
     function addbook() {
       if(formData.bookname === null || formData.bookyear === null || formData.price === null) {
@@ -39,7 +35,7 @@ export default {
       } else {
         watch(() => {
           emit('addbook', [formData.bookname, formData.bookyear, formData.price]);
-        formData.bookname = formData.bookyear = formData.price = '';
+          formData.bookname = formData.bookyear = formData.price = '';
         })
       }
     }
@@ -53,5 +49,8 @@ export default {
 <style scoped>
 label {
   color: red;
+}
+form {
+  width: 50%;
 }
 </style>>
